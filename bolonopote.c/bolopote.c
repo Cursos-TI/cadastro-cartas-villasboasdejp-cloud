de3bb2ab54b1f46ba552e9769d77310054918902
Abner <villasboasdejp@gmail.com>
2026-05-11 21:01:02 -0300

    bolo

---
+++ bolonopote.c/bolopote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+	int main(){
+
+
+	//*apresentação para o cliente
+
+	printf("Ola,Seja Bem vindo \n");
+	printf("        ao         \n");
+	printf("    VILLAS DOCE    \n");
+}
